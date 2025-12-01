53329690a31baef30de39d5e41563915fdfa5fe7
Sumayya <sumayyakhalid923@gmail.com>
2025-12-02 01:39:33 +0530

    before img import

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -3,9 +3,44 @@ import HeroImg from "../assets/rose2.jpg";
 
 function Hero() {
   return (
-    <div className="hero  ">
-      <img className="w-full" src={HeroImg} alt="Hero" />
-      <h1>Welcome to Our Site</h1>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "80vh", // adjust height
+        backgroundImage: `url(${HeroImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="hero  "
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "20%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <h1 style={{ fontSize: "3rem", marginBottom: "20px" }}>
+          Welcome to Our Site
+        </h1>
+        <button
+          style={{
+            padding: "10px 20px",
+            fontSize: "1.2rem",
+            border: "none",
+            borderRadius: "5px",
+            backgroundColor: "#ff7f50",
+            color: "white",
+            cursor: "pointer",
+          }}
+        >
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
